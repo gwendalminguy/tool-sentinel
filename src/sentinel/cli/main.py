3ded65b55e82bcc1f03459994aab11f08062c39b
Gwendal <gwendalminguy@outlook.fr>
2026-06-12 21:18:17 +0200

    Create ask and chat commands

--- src/sentinel/cli/main.py
+++ src/sentinel/cli/main.py
@@ -4,12 +4,35 @@ Sentinel CLI Entry Point
 """
 from sentinel.llm.client import OllamaClient
 
+import typer
 
-def main():
+
+app = typer.Typer()
+
+@app.command()
+def ask(prompt: str = typer.Argument(..., help="Question to ask to the LLM.")):
+    """
+    Ask the LLM a question and print the response.
+    """
     client = OllamaClient()
-    response = client.generate("Hello.")
+    response = client.generate(prompt)
+
     print(response)
 
 
+@app.command()
+def chat():
+    """
+    Start an interactive chat session with the LLM.
+    """
+    client = OllamaClient()
+
+    while True:
+        prompt = input("> ")
+        response = client.generate(prompt)
+
+        print(f"> {response}")
+
+
 if __name__ == "__main__":
-    main()
+    app()
