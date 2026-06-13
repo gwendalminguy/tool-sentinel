364ebc300d189129944e787cdf8e7966420ac59a
Gwendal <gwendalminguy@outlook.fr>
2026-06-13 12:37:21 +0200

    Create iter_files utility function

---
+++ src/sentinel/utils/filesystem.py
@@ -0,0 +1,21 @@
+"""
+sentinel/utils/filesystem.py
+FileSystem Utility Functions
+"""
+import os
+
+
+def iter_files(path: str):
+    """
+    Yield all file paths from a directory tree.
+
+    If `path` is a directory, recursively yields all file paths inside it.
+    If `path` is a file, yields the file itself.
+    """
+    if os.path.isdir(path):
+        for root, _, files in os.walk(path):
+            for file in files:
+                yield os.path.join(root, file)
+
+    else:
+        yield path
